3aaf30db3e62b65ed0ae02a3b25e559f3530d4af
vi nguyen <nguyenduongthevi31415@gmail.com>
2024-06-13 11:41:11 +0700

    Update

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { Helmet } from "react-helmet";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <GoogleOAuthProvider clientId="939314138120-82cp645chuac36hbddg6tq6uh8jmtpdi.apps.googleusercontent.com">
+  <GoogleOAuthProvider clientId="780332756678-s91uf0kq1och63944jrd88vii16nmusq.apps.googleusercontent.com">
     <React.StrictMode>
       <Helmet>
         <title>EduArchive</title>
