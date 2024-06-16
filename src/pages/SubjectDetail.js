dd4f568b8cbd5e4272b86ab9061d0d38d62bd4c2
vi nguyen <nguyenduongthevi31415@gmail.com>
2024-06-16 16:39:03 +0700

    Update

--- src/pages/SubjectDetail.js
+++ src/pages/SubjectDetail.js
@@ -1,35 +1,49 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { ClipLoader } from "react-spinners";
-
+import { Link } from "react-router-dom";
+import DocumentCard from "../component/DocumentCard";
+import { ToastContainer, toast } from "react-toastify";
+import ExamCard from "../component/ExamCard";
+import ProjectCard from "../component/ProjectCard";
 function SubjectDetail() {
   const { subjectId } = useParams();
   const navigate = useNavigate();
-
   const [subject, setSubject] = useState(null);
   const [loading, setLoading] = useState(true);
   const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
-  const [viewerUrl, setViewerUrl] = useState("");
-  const getFileExtension = (url) => {
-    return url.split(".").pop().toLowerCase();
-  };
-
+  const [documents, setDocuments] = useState([]);
   useEffect(() => {
-    setLoading(true);
+    // Fetch subject details
     fetch(`${API_BASE_URL}/api/Subject/ById/${subjectId}`)
       .then((res) => res.json())
       .then((data) => {
         if (data.data) {
           setSubject(data.data);
         } else {
-          // navigate("/notfound"); // Chuyển hướng đến trang NotFound nếu document là null
+          navigate("/notfound"); // Chuyển hướng đến trang NotFound nếu document là null
         }
-        setLoading(false);
       })
       .catch(() => {
-        // navigate("/notfound"); // Chuyển hướng đến trang NotFound nếu có lỗi
+        navigate("/notfound"); // Chuyển hướng đến trang NotFound nếu có lỗi
       });
-  }, [subjectId, API_BASE_URL, navigate]);
+
+    fetch(`${API_BASE_URL}/api/Document/search?idSubject=${subjectId}`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.status !== true) {
+          toast.error("Không thể tìm kiếm !");
+        } else {
+          setDocuments(data.data);
+          toast.success("Tìm kiếm thành công !");
+        }
+      })
+      .catch((error) => {
+        toast.error(error);
+      });
+    setLoading(false);
+  }, [subjectId]);
+
   if (loading) {
     return (
       <div
@@ -44,14 +58,7 @@ function SubjectDetail() {
       </div>
     );
   }
-  const formatDate = (dateString) => {
-    const date = new Date(dateString);
-    const day = date.getDate();
-    const month = date.getMonth() + 1;
-    const year = date.getFullYear();
-    return `${day}/${month}/${year}`;
-  };
-  const formattedDate = formatDate(document.createDate);
+
   return (
     <>
       <nav
@@ -80,75 +87,240 @@ function SubjectDetail() {
             <span>/</span>
           </li>
           <li>
-            <span className="font-bold">{subject.name}</span>
+            <span className="font-bold">{subject?.name}</span>
           </li>
         </ol>
       </nav>
 
-      <div className="container mx-auto m-5">
+      <div className="container mx-auto my-5 px-4">
         <div
-          style={{
-            backgroundColor: "#effbdd",
-            padding: "20px",
-            borderRadius: "15px",
-            paddingRight: "50px",
-            paddingLeft: "50px",
-          }}
+          className=" rounded-2xl gradient-background"
+          style={{ borderWidth: "1px" }}
         >
-          <div>
-            <div className="flex items-center space-x-4">
-              <i
-                className="fa-solid fa-book fa-5x"
-                style={{ color: "#40D174", margin: "10px" }}
-              ></i>
-              <div>
-                <p
-                  className="text-lg font-semibold"
-                  style={{
-                    color: "#48DA7D",
-                    fontSize: "30px",
-                    fontWeight: "500",
-                  }}
-                >
-                  {subject.name}
-                </p>
-                <p
-                  style={{
-                    marginTop: "10px",
-                    color: "#323f48",
-                    fontSize: "20px",
-                    fontWeight: "bold",
-                  }}
-                >
-                  {subject.code}
-                </p>
-                <p className=" text-gray-800 text-lg">
-                  <div className="flex items-center space-x-4">
-                    <div className="flex items-center space-x-1 text-green-500">
-                      <i className="fa-solid fa-file"></i>
-                      <span style={{ color: "#323f48", fontWeight: "bold" }}>
-                        {subject.document}
-                      </span>
-                    </div>
-                    <div className="flex items-center space-x-1 text-green-500">
-                      <i className="fa-solid fa-eye"></i>
-                      <span style={{ color: "#323f48", fontWeight: "bold" }}>
-                        {subject.view}
-                      </span>
-                    </div>
-                    <div className="flex items-center space-x-1 text-green-500">
-                      <i className="fa-solid fa-heart"></i>
-                      <span style={{ color: "#323f48", fontWeight: "bold" }}>
-                        {subject.like}
-                      </span>
-                    </div>
-                  </div>
-                </p>
+          <div className="relative">
+            <img
+              src={require("./images/bg_green.jpg")}
+              alt="Cover"
+              className="w-full h-40 object-cover rounded-t-2xl"
+            />
+            <div className="absolute top-24 w-full flex justify-center md:left-8 md:w-auto">
+              <img
+                src={subject?.avartar}
+                alt="Profile"
+                className=" rounded-lg object-cover border-4 border-white shadow-md"
+                style={{ width: "120px", height: "120px" }}
+              />
+            </div>
+          </div>
+          <div
+            className=" px-5 md:px-8 py-5 flex flex-col md:flex-row justify-between items-center"
+            style={{ marginTop: "55px" }}
+          >
+            <div className="flex flex-col text-center md:text-left">
+              <h1
+                className=" text-3xl"
+                style={{ color: "#191919", fontWeight: "500" }}
+              >
+                {subject?.name}
+              </h1>
+              <p className="mt-2 text-xl  text-gray-800">{subject?.code}</p>
+              <div className="flex justify-center md:justify-start mt-3 md:px-0">
+                <button className=" bg-green-400  hover:bg-green-500 text-white font-bold py-2 px-4 rounded-lg">
+                  <i class="fa-solid fa-plus"></i> Theo dõi môn học
+                </button>
               </div>
             </div>
-            <p style={{ marginTop: "10px", color: "#323f48" }}>
-              {subject.description}
-            </p>
+
+            <div
+              className="flex items-center space-x-6 mt-3 md:mt-0 text-gray-800"
+              style={{ fontSize: "23px" }}
+            >
+              <div className="flex items-center space-x-1">
+                <i className="fa-solid fa-file text-green-500"></i>
+                <span className="font-bold">{subject?.document}</span>
+              </div>
+              <div className="flex items-center space-x-1">
+                <i className="fa-solid fa-eye text-green-500"></i>
+                <span className="font-bold">{subject?.view}</span>
+              </div>
+              <div className="flex items-center space-x-1">
+                <i className="fa-solid fa-heart text-green-500"></i>
+                <span className="font-bold">{subject?.like}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-0 mt-6">
+          {/* Cột bên trái */}
+          <div className="col-span-2 space-y-6">
+            {/* Mục Giới thiệu */}
+            <div className="rounded-2xl gradient-background border-2 p-6">
+              <p className="text-gray-800 text-center md:text-left text-lg font-semibold mb-4">
+                <i className="fa-solid fa-circle-info mr-2 text-green-500"></i>
+                Giới thiệu
+              </p>
+              <p className="text-gray-800">{subject?.description}</p>
+            </div>
+
+            {/* Mục Tài liệu */}
+            <div className="rounded-2xl gradient-background border-2 p-6">
+              <p className="text-gray-800 text-center md:text-left text-lg font-semibold mb-4">
+                <i className="fa-solid fa-folder mr-2 text-green-500"></i>Tài
+                liệu
+              </p>
+              {documents.length > 0 ? (
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
+                  {documents
+                    .filter((document) => document.typeDocument === "TaiLieu")
+                    .map((document) => (
+                      <div key={document.id}>
+                        <Link to={`/document/${document.id}`}>
+                          <DocumentCard document={document} />
+                        </Link>
+                      </div>
+                    ))}
+                </div>
+              ) : (
+                <div
+                  className="flex items-center justify-center "
+                  style={{ margin: "20px" }}
+                >
+                  <div className="text-center">
+                    <p
+                      className="text-lg font-bold mb-2"
+                      style={{ fontSize: "30px", color: "#70DE92" }}
+                    >
+                      Không tìm thấy tài liệu !
+                    </p>
+                    <p className="text-gray-600">
+                      Xin lỗi, hiện tại chưa có tài liệu nào. Vui lòng quay trở
+                      lại sau !
+                    </p>
+                  </div>
+                </div>
+              )}
+            </div>
+
+            {/* Mục Đề thi */}
+            <div className="rounded-2xl gradient-background border-2 p-6">
+              <p className="text-gray-800 text-center md:text-left text-lg font-semibold mb-4">
+                <i className="fa-solid fa-folder mr-2 text-green-500"></i>Đề thi
+              </p>
+              {documents.length > 0 ? (
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
+                  {documents
+                    .filter((document) => document.typeDocument === "DeThi")
+                    .map((document) => (
+                      <div key={document.id}>
+                        <Link to={`/exam/${document.id}`}>
+                          <ExamCard document={document} />
+                        </Link>
+                      </div>
+                    ))}
+                </div>
+              ) : (
+                <div
+                  className="flex items-center justify-center "
+                  style={{ margin: "20px" }}
+                >
+                  <div className="text-center">
+                    <p
+                      className="text-lg font-bold mb-2"
+                      style={{ fontSize: "30px", color: "#70DE92" }}
+                    >
+                      Không tìm thấy tài liệu !
+                    </p>
+                    <p className="text-gray-600">
+                      Xin lỗi, hiện tại chưa có tài liệu nào. Vui lòng quay trở
+                      lại sau !
+                    </p>
+                  </div>
+                </div>
+              )}
+            </div>
+
+            {/* Mục Đồ án */}
+            <div className="rounded-2xl gradient-background border-2 p-6">
+              <p className="text-gray-800 text-center md:text-left text-lg font-semibold mb-4">
+                <i className="fa-solid fa-folder mr-2 text-green-500"></i>Đồ án
+              </p>
+              {documents.length > 0 ? (
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
+                  {documents
+                    .filter((document) => document.typeDocument === "DoAn")
+                    .map((document) => (
+                      <div key={document.id}>
+                        <Link to={`/project/${document.id}`}>
+                          <ProjectCard document={document} />
+                        </Link>
+                      </div>
+                    ))}
+                </div>
+              ) : (
+                <div
+                  className="flex items-center justify-center "
+                  style={{ margin: "20px" }}
+                >
+                  <div className="text-center">
+                    <p
+                      className="text-lg font-bold mb-2"
+                      style={{ fontSize: "30px", color: "#70DE92" }}
+                    >
+                      Không tìm thấy tài liệu !
+                    </p>
+                    <p className="text-gray-600">
+                      Xin lỗi, hiện tại chưa có tài liệu nào. Vui lòng quay trở
+                      lại sau !
+                    </p>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+
+          {/* Cột bên phải */}
+          <div className="col-span-1 md:ml-6 md:mt-0 mt-6 border-2 rounded-2xl">
+            <div className="h-full  rounded-2xl p-6  items-center">
+              {/* <div className="rounded-2xl gradient-background border-2 p-6">
+                <p className="text-gray-800 text-center md:text-left text-lg font-semibold mb-4">
+                  <i className="fa-solid fa-folder mr-2 text-green-500"></i>Đồ
+                  án
+                </p>
+                <p className="text-gray-800">{subject?.description}</p>
+              </div> */}
+              <div className="relative flex-1 max-w-xl mt-4 sm:mt-0 w-full sm:w-auto flex justify-end">
+                <div className="relative  max-w-xl" style={{ width: "100%" }}>
+                  <input
+                    placeholder="Tìm kiếm tài liệu..."
+                    className="rounded-full w-full h-16 bg-transparent py-1 pl-8 pr-10 outline-none border-2 border-gray-100 shadow-md hover:outline-none focus:ring-green-200 focus:border-green-200"
+                    type="text"
+                    name="query"
+                    id="query"
+                  />
+                  <button
+                    type="submit"
+                    // onClick={handleSearch}
+                    className="absolute inline-flex items-center h-10 px-4 py-2 text-sm text-white transition duration-150 ease-in-out rounded-full outline-none right-3 top-3 bg-green-400 sm:px-6 sm:text-base sm:font-medium hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+                  >
+                    <svg
+                      className="-ml-0.5 sm:-ml-1 mr-2 w-4 h-4 sm:h-5 sm:w-5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                      />
+                    </svg>
+                    Search
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
