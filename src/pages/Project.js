dd985cec830eda4ee72236a392e78ec051e1357f
vi nguyen <nguyenduongthevi31415@gmail.com>
2024-06-19 19:44:46 +0700

    Update

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -175,7 +175,11 @@ function Project() {
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4 px-4">
               {documents.map((document) => (
                 <div key={document.id}>
-                  <Link to={`/project/${document.id}`}>
+                  <Link
+                    to={`/project/${document.id}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     <ProjectCard document={document} />
                   </Link>
                 </div>
